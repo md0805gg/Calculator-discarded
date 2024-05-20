a44bef472d7ab91c9efa9b663a5f7d6247fe32fd
md0805gg-linux <112896171+md0805gg@users.noreply.github.com>
2024-05-20 09:07:29 +0200

    Create operate function
    Function designed to consider selected operator
    while evaluating the outcome

--- script.js
+++ script.js
@@ -17,3 +17,20 @@ function multiply (a, b) {
 function divide (a, b) {
   return a / b
 };
+
+function operate (a, b) {
+  switch (operator) {
+    case '+':
+      add (a, b);
+      break;
+    case '-':
+      subtract (a, b);
+      break;
+    case '*':
+      multiply (a, b);
+      break;
+    case '/':
+      divide (a, b);
+      break;
+  };
+};
