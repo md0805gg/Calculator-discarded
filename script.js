92d3e11af954acbbd00e3e44889b1ceb7666f2e2
md0805gg-linux <112896171+md0805gg@users.noreply.github.com>
2024-05-22 16:42:56 +0200

    Prepare calculator object & add pseudocode
    Start shaping calculator as an object.
    Prepare initial keys that will be used in
    logic designed later.
    Add pseudocode for planning.

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-let numberOne;
+let numberOne
 let operator;
 let numberTwo;
 let displayValue = document.querySelector('.display p');
 let buttons = document.querySelectorAll('button');
 
+let calculator = {
+  numberOne,
+  numberTwo,
+  operator,
+  displayValue
+};
+
 buttons.forEach((button) => {
   button.addEventListener('click', () => 
   displayValue.textContent = button.textContent);
 });
+
+//display value as object?
+//first, push all clicked numbers to an array nubmerOne;
+// value => array => push => array to value
+//each time you add, transform array into the number;
+//once you click an operator button display it as well
+//and store all the values in numberTwo array
+//once you click = fire up operate
 
 function add (a, b) {
   return a + b
